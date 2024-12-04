60ce499038374945e43e277d3d2f451f570a9669
jmvantreeck <100625388+jmvantreeck@users.noreply.github.com>
2024-12-03 23:02:49 -0600

    Update PushArtifacts

--- RL-Catan-main/RL_agent/Artifcats.py
+++ RL-Catan-main/RL_agent/Artifcats.py
@@ -1,25 +1,28 @@
 import pandas as pd
 import numpy as np
 import mlflow
+import torch
 
-def PushArtifacts(experiment_name, params,Model,Results,MLFLOW_ADDRESS,TrainingData=False,TestingData = False):
+def PushArtifacts(experiment_name, params,Model,Results,MLFLOW_ADDRESS,TrainingData=False,TestingData = False,TagID = 0):
     if (TrainingData == False and TestingData == False) or (TrainingData == True and TestingData == True):
+        print("Error: Either TrainingData or TestingData must be True, but not both.")
         return 0
     DF = pd.DataFrame(Results)
-    DF.to_csv('Results.csv')
+    DF.to_csv('Artifacts/Results.csv')
+    torch.save(Model.state_dict(),'Artifacts/Model_Parameters.pth')
     mlflow.set_tracking_uri(uri=MLFLOW_ADDRESS)
     mlflow.set_experiment(experiment_name)
     with mlflow.start_run():
         mlflow.log_params(params)
+        mlflow.log_artifact('Results.csv')
+        mlflow.log_artifact('Model_Parameters.pth')
         if TrainingData is True:
-            mlflow.log_artifact('Results.csv')
             mlflow.log_metric('average moves', np.mean(DF.moves))
             mlflow.log_metric('average reward per move', np.mean(DF.RewardsPerTurn))
             mlflow.log_metric('average loss', np.mean(DF.AverageQloss))
-            mlflow.set_tag("Training Run", "Test that ML Flow is working...")
+            mlflow.set_tag("Training Run", str(TagID))
         if TestingData is True:
-            mlflow.log_artifact('Results.csv')
             mlflow.log_metric('Win rate',DF.winrate)
             mlflow.log_metric('RL Avg Victroy Points ', DF.VP0)
             mlflow.log_metric('Random Avg Victroy Points ', DF.VP1)
-            mlflow.set_tag("Training Run", "Test that ML Flow is working...")
+            mlflow.set_tag("Testing Run", str(TagID))
