b426e5e2d110a757cce37f7a7048fc362b07d696
Wasif Islam <islamwasif3@gmail.com>
2024-12-06 16:10:34 -0500

    feat(dramatiq): Implement dramatiq broker, and s3 bucket function for testing

---
+++ packages/catan-dramatiq/src/catan_dramatiq/broker/core.py
@@ -0,0 +1,30 @@
+import dramatiq
+from dramatiq.brokers.redis import RedisBroker
+from dramatiq.results import Results
+from dramatiq.results.backends import RedisBackend
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+from redis import Redis
+
+class AppSettings(BaseSettings):
+    """Import all environment variables passed to pod and initialize the password for redis"""
+    redis_password: SecretStr = SecretStr('default')
+
+
+##################### Execution #######################
+
+app_settings = AppSettings()
+
+redis_client = Redis(
+    host="chart-redis-master.catan.svc.cluster.local",
+    password=app_settings.redis_password.get_secret_value(),
+    port=6379,
+    decode_responses=False,
+    socket_connect_timeout=0.1,
+)
+
+results_backend = Results(backend=RedisBackend(client=redis_client))
+
+redis_broker = RedisBroker(client=redis_client)
+redis_broker.add_middleware(results_backend)
+dramatiq.set_broker(broker=redis_broker)
---
+++ packages/catan-dramatiq/src/catan_dramatiq/storage/s3storage.py
@@ -0,0 +1,31 @@
+from minio import Minio
+from io import BytesIO
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+
+class MinioSettings(BaseSettings):
+    minio_user: str = 'admin'
+    minio_password: SecretStr = SecretStr('default')
+
+
+def create_s3_bucket(minio_client: Minio, bucket_name: str) -> str:
+    bucket_exist = minio_client.bucket_exists(bucket_name)
+    if not bucket_exist:
+        minio_client.make_bucket(bucket_name)
+        return f"LOG: Success - Created Bucket: {bucket_name}"
+    return f"LOG: Info - Bucket Exists: {bucket_name}"
+
+def upload_file_from_fs( minio_client: Minio, bucket_name: str, obj_name: str, file_path: str) -> None:
+    minio_client.fput_object(bucket_name, obj_name, file_path)
+    
+
+######### Execution ##########
+
+minio_settings = MinioSettings()
+minio_client = Minio(
+    'chart-minio:9001',
+    access_key=minio_settings.minio_user,
+    secret_key=minio_settings.minio_password.get_secret_value(),
+    secure=False,
+)
+
