09951e47ca0b1f7429abbfc0d878eeca1c3d3e1f
Wasif Islam <islamwasif3@gmail.com>
2024-12-07 02:07:10 -0500

    fix(web): removing catam_ml from deps

--- packages/catan-dramatiq/src/catan_dramatiq/tasks/tasks.py
+++ packages/catan-dramatiq/src/catan_dramatiq/tasks/tasks.py
@@ -1,6 +1,9 @@
 #import json
 import dramatiq
-from catan_dramatiq.broker import redis_broker
+from catan_dramatiq.broker.core import redis_broker, results_backend
+
+redis_broker.add_middleware(results_backend)
+dramatiq.set_broker(broker=redis_broker)
 
 @dramatiq.actor(broker=redis_broker)
 def create_s3_bucket(bucket_name: str) -> str:
