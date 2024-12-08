dde193bfb23754cf834ad92b5ce39e0e607a433d
Wasif Islam <islamwasif3@gmail.com>
2024-12-07 22:12:57 -0500

    chore(dramatiq): actually include the actor function from previous commit

--- packages/catan-dramatiq/src/catan_dramatiq/tasks/tasks.py
+++ packages/catan-dramatiq/src/catan_dramatiq/tasks/tasks.py
@@ -4,6 +4,7 @@ from typing import Any
 from catan_dramatiq.broker.core import redis_broker, results_backend
 from catan_dramatiq.catan_env.game import Game
 from catan_dramatiq.storage import mlflow
+from catan_dramatiq.orm.config import TrainingInput
 
 redis_broker.add_middleware(results_backend)
 dramatiq.set_broker(broker=redis_broker)
@@ -22,10 +23,11 @@ def receive_pydantic_model(config: dict[str, Any]) -> str:
 
 @dramatiq.actor(broker=redis_broker)
 def execute_rl_benchmark(config: dict[str, Any]) -> str:
+
+    # Setup Device and Config
     torch.manual_seed(2)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    param_config = TrainingInput.model_validate(config)
 
-
-
-    return "Successful BenchMark"
+    return f"Device: {device}, URI: {param_config.MLFLOW_ADDRESS}"
 
