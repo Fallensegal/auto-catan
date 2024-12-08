cd57c13b8fdc82c4c02e7412f966b7b0b4117365
Wasif Islam <islamwasif3@gmail.com>
2024-12-07 22:03:03 -0500

    feat(ml): implemented stateful classes for environment and executor

--- packages/catan-dramatiq/src/catan_dramatiq/tasks/tasks.py
+++ packages/catan-dramatiq/src/catan_dramatiq/tasks/tasks.py
@@ -1,8 +1,9 @@
-#import json
 import dramatiq
+import torch
 from typing import Any
 from catan_dramatiq.broker.core import redis_broker, results_backend
-#from catan_dramatiq.orm.config import TrainingInput
+from catan_dramatiq.catan_env.game import Game
+from catan_dramatiq.storage import mlflow
 
 redis_broker.add_middleware(results_backend)
 dramatiq.set_broker(broker=redis_broker)
@@ -18,3 +19,13 @@ def create_s3_bucket(bucket_name: str) -> str:
 @dramatiq.actor(broker=redis_broker)
 def receive_pydantic_model(config: dict[str, Any]) -> str:
     return config['MLFLOW_ADDRESS']
+
+@dramatiq.actor(broker=redis_broker)
+def execute_rl_benchmark(config: dict[str, Any]) -> str:
+    torch.manual_seed(2)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+
+    return "Successful BenchMark"
+
